bcee1e9bd2c6e1b149f5f197b478d79ced846500
LuckyJayce <794629068@qq.com>
2018-10-28 14:17:56 +0800

    从MVCHelper迁移到这

---
+++ app/src/main/java/com/shizhefei/task/demo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.shizhefei.task.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.example.luckyjayce.task.R.layout.activity_main);
+    }
+}
---
+++ mvchelper_task_core/src/main/java/com/shizhefei/task/ISuperTask.java
@@ -0,0 +1,8 @@
+package com.shizhefei.task;
+
+/**
+ * 顶层抽象的task，标识接口
+ * Created by LuckyJayce on 2016/7/17.
+ */
+public interface ISuperTask<DATA>{
+}
