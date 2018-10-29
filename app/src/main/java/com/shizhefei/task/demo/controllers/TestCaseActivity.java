a65f4f51256da748c195115283188be97715dce3
LuckyJayce <794629068@qq.com>
2018-10-29 23:18:18 +0800

    添加测试用例界面

--- app/src/main/java/com/shizhefei/task/demo/controllers/TestCaseActivity.java
+++ app/src/main/java/com/shizhefei/task/demo/controllers/TestCaseActivity.java
@@ -1,13 +1,17 @@
 package com.shizhefei.task.demo.controllers;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.shizhefei.mvchelper.testcase.ABSTestCaseFragment;
 import com.shizhefei.mvchelper.testcase.TestCaseData;
 import com.shizhefei.task.demo.R;
+import com.shizhefei.task.demo.models.task.download.DownloadSyncTask;
 import com.shizhefei.task.demo.models.task.login.LoginSyncTask;
+import com.shizhefei.task.demo.models.task.register.RegisterTask;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,6 +29,8 @@ public class TestCaseActivity extends AppCompatActivity {
         protected List<TestCaseData> getTestCaseDatas() {
             List<TestCaseData> data = new ArrayList<>();
             data.add(new TestCaseData("登录", new LoginSyncTask("小明", "111111")));
+            data.add(new TestCaseData("注册", new RegisterTask("小明", 10, "111111", Environment.getExternalStorageDirectory() + File.separator + "a.png")));
+            data.add(new TestCaseData("下载", new DownloadSyncTask("url", "path")));
             return data;
         }
     }
