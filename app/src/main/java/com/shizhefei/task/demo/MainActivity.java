b173bb592a61f30389007f44744f2982b64f9345
LuckyJayce <794629068@qq.com>
2018-10-29 13:17:45 +0800

    从MVCHelper迁移到这

--- app/src/main/java/com/shizhefei/task/demo/MainActivity.java
+++ app/src/main/java/com/shizhefei/task/demo/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(com.example.luckyjayce.task.R.layout.activity_main);
+        setContentView(R.layout.activity_main);
     }
 }
---
+++ mvchelper_task_core/src/main/java/com/shizhefei/task/ProxyTask.java
@@ -0,0 +1,18 @@
+package com.shizhefei.task;
+
+import com.shizhefei.mvc.RequestHandle;
+import com.shizhefei.mvc.ResponseSender;
+
+/**
+ * Created by luckyjayce on 2017/4/16.
+ */
+
+public abstract class ProxyTask<DATA> implements IAsyncTask<DATA> {
+
+    @Override
+    public final RequestHandle execute(ResponseSender<DATA> sender) throws Exception {
+        return getTask().execute(sender);
+    }
+
+    protected abstract IAsyncTask<DATA> getTask();
+}
