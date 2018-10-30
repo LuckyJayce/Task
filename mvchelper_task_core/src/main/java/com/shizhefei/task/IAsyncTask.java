86816d7f86a23174cc192e9e968e2d481c1dd6cc
LuckyJayce <794629068@qq.com>
2018-10-30 19:13:32 +0800

    修改注释信息

--- mvchelper_task_core/src/main/java/com/shizhefei/task/IAsyncTask.java
+++ mvchelper_task_core/src/main/java/com/shizhefei/task/IAsyncTask.java
@@ -4,14 +4,14 @@ import com.shizhefei.mvc.RequestHandle;
 import com.shizhefei.mvc.ResponseSender;
 
 /**
- * 异步Task，不能直接执行超时任务，可以执行像OKhttp异步回调那样
+ * 异步Task，需要自己开线程执行异步操作，execute的方法在主线程执行，不能执行超时任务
  * Created by LuckyJayce on 2016/7/17.
  */
 public interface IAsyncTask<DATA> extends ISuperTask<DATA> {
-    
+
     /**
-     * @param sender 用于请求结束时发送数据给TaskHelper,MVCHelper,然后在通知CallBack的Post回调方法
-     * @return 用于提供外部取消请求的处理
+     * @param sender 发送进度或者结果数据，回调ICallback的onProgress或者onPost
+     * @return 返回给外面一个取消句柄，外面通过这个执行取消
      * @throws Exception
      */
     RequestHandle execute(ResponseSender<DATA> sender) throws Exception;
--- mvchelper_task_core/src/main/java/com/shizhefei/task/ITask.java
+++ mvchelper_task_core/src/main/java/com/shizhefei/task/ITask.java
@@ -4,12 +4,20 @@ package com.shizhefei.task;
 import com.shizhefei.mvc.ProgressSender;
 
 /**
- * 同步task，直接执行耗时的任务，直接返回数据
+ * 同步Task，不需要自己开线程，execute的方法在后台执行可以执行超时任务
  * Created by LuckyJayce on 2016/7/17.
  */
 public interface ITask<DATA> extends ISuperTask<DATA> {
 
+    /**
+     * @param progressSender 发送进度数据
+     * @return 返回结果数据
+     * @throws Exception 失败的时候通过异常抛出
+     */
     DATA execute(ProgressSender progressSender) throws Exception;
 
+    /**
+     * 外面通过这个执行取消
+     */
     void cancel();
 }
