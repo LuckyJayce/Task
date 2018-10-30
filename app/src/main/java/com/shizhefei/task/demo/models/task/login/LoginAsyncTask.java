3ed3418d600402c2c184f6ab23f722aecae031e6
LuckyJayce <794629068@qq.com>
2018-10-30 19:15:21 +0800

    修改注释

--- app/src/main/java/com/shizhefei/task/demo/models/task/login/LoginAsyncTask.java
+++ app/src/main/java/com/shizhefei/task/demo/models/task/login/LoginAsyncTask.java
@@ -15,18 +15,19 @@ public class LoginAsyncTask implements IAsyncTask<User> {
         this.password = password;
     }
 
+    //这个函数是在主线程调用执行，不可以执行超时的任务
+    // 可以自己开线程或者其它框架有线程，返回结果后再通过调用sender在异步线程调用返回结果
     @Override
     public RequestHandle execute(ResponseSender<User> sender) throws Exception {
-        //这个函数是在主线程调用执行，不可以执行超时的任务
-        // 可以自己开线程或者其它框架有线程，返回结果后再通过调用sender在异步线程调用返回结果
-
         if (!password.equals("111111")) {
-            //错误的情况通过异常跑出
+            //错误的情况通过异常抛出
             sender.sendError(new AuthException("密码不正确"));
         } else {
             //返回正确的结果
             sender.sendData(new User(name, 11));
         }
+        //返回取消的句柄，如果不需要取消逻辑直接返回null，如果有的话实现RequestHandle
         return null;
     }
+
 }
